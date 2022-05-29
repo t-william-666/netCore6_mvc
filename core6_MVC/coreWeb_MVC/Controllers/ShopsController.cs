8c373d9c7f4a84e9beaf26d3e9eafe88a7b0be64
t-william-666 <1839368291@qq.com>
2022-05-30 00:25:41 +0800

    更新nuget包

---
+++ core6_MVC/coreWeb_MVC/Controllers/ShopsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using coreWeb_MVC.Models;
+
+namespace coreWeb_MVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopsController : ControllerBase
+    {
+        private readonly TestDBContext _context;
+
+        public ShopsController(TestDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Shops
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Shop>>> GetShops()
+        {
+          if (_context.Shops == null)
+          {
+              return NotFound();
+          }
+            return await _context.Shops.ToListAsync();
+        }
+
+        // GET: api/Shops/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Shop>> GetShop(string id)
+        {
+          if (_context.Shops == null)
+          {
+              return NotFound();
+          }
+            var shop = await _context.Shops.FindAsync(id);
+
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            return shop;
+        }
+
+        // PUT: api/Shops/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutShop(string id, Shop shop)
+        {
+            if (id != shop.ShopID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(shop).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ShopExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Shops
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Shop>> PostShop(Shop shop)
+        {
+          if (_context.Shops == null)
+          {
+              return Problem("Entity set 'TestDBContext.Shops'  is null.");
+          }
+            _context.Shops.Add(shop);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ShopExists(shop.ShopID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetShop", new { id = shop.ShopID }, shop);
+        }
+
+        // DELETE: api/Shops/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteShop(string id)
+        {
+            if (_context.Shops == null)
+            {
+                return NotFound();
+            }
+            var shop = await _context.Shops.FindAsync(id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            _context.Shops.Remove(shop);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ShopExists(string id)
+        {
+            return (_context.Shops?.Any(e => e.ShopID == id)).GetValueOrDefault();
+        }
+    }
+}
